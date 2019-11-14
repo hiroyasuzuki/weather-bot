6febd856526163c43341efa05d441cace13fc17b
YourName <you@example.com>
2019-11-14 15:27:21 +0900

    Initial Commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,14 @@
+class ApplicationController < ActionController::Base
+  require_relative 'boot'
+
+  require 'rails/all'
+  Bundler.require(*Rails.groups)
+
+  module Railnotice
+    class Application < Rails::Application
+      config.load_defaults 5.2
+      config.time_zone = 'Tokyo'
+      config.i18n.default_locale = :ja
+    end
+  end
+end
